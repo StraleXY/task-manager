ee2da269bf116d75646c46fa471a3ef921d28861
Strahinja Sekulic <strahinja0123@gmail.com>
2023-12-29 12:15:34 +0100

    Add: Hilt

---
+++ app/src/main/java/com/theminimalismhub/taskmanager/di/AppModule.kt
@@ -0,0 +1,16 @@
+package com.theminimalismhub.taskmanager.di
+
+import android.app.Application
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides @Singleton fun providesContext(app: Application) : Context = app.applicationContext
+
+}
---
+++ app/src/main/java/com/theminimalismhub/taskmanager/di/TaskManagerApp.kt
@@ -0,0 +1,7 @@
+package com.theminimalismhub.taskmanager.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TaskManagerApp : Application()
